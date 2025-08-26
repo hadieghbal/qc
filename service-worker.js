9db44264a9c43d39ec0391cc760cc701aa9b49cd
hadieghbal <hadieghbal69@gmail.com>
2025-08-26 17:21:31 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // نام و نسخه جدید کش
-const CACHE_NAME = "qc-app-cache-v7";
+const CACHE_NAME = "qc-app-cache-v8";
 
 // لیست تمام فایل‌های ضروری برنامه با آدرس‌های اصلاح شده
 const FILES_TO_CACHE = [
