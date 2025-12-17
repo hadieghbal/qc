d5388cbfe6138a501f372f7a257f1f22b88002d3
hadieghbal <hadieghbal69@gmail.com>
2025-12-17 16:56:04 +0330

    up

--- features/home/forms/checklists/checklist-injection/checklist-injection.js
+++ features/home/forms/checklists/checklist-injection/checklist-injection.js
@@ -35,7 +35,7 @@ export function init() {
   let finalizedData = {};
   let mobileDeviceChoice;
 
-  // نگهداری اینستنس‌های Choices
+  /// نگهداری اینستنس‌های Choices
   let partChoices = new Map();
   let materialChoices = new Map();
   let masterbatchChoices = new Map();
